c55ceb170c367f78b08402a151d8ce8a93cafeea
Hamam Massarweh <hamam.massarweh@gmail.com>
2017-11-19 23:56:07 +0200

    print nullable

--- b/hw2b.cpp
+++ b/hw2b.cpp
@@ -1,41 +1,48 @@
 #include "grammar.h"
+#include <iostream>
 using namespace std;
 
-bool checkIfNullable(grammar_rule nonTerminal){
-	
+bool checkIfNullable(nonterminal nonTerminal) {
 	
 	//checking if this non terminal derives null directly.
-	vector<int> _rhs = nonTerminal->rhs;
-	
-	
-	for(
-	
-	return true;
-	
-	
-	
-	
-	
+	cout << (int)nonTerminal << endl;
+	if (nonTerminal >= 10)
+		return false;
+
+	for (int i = 0; i < grammar.size(); ++i)
+		if (grammar[i].lhs == nonTerminal)
+			if (grammar[i].rhs.size() == 0)
+				return true;
+			
+
+	for (int i = 0; i < grammar.size(); ++i)
+	{
+		if (grammar[i].lhs == nonTerminal)
+		{
+			bool flag = true;
+
+			for (int j = 0; j < grammar[i].rhs.size(); ++j)
+			{
+				if (grammar[i].rhs[j] != (int)nonTerminal)
+					flag = flag && checkIfNullable((nonterminal)(grammar[i].rhs[j]));
+			}
+			if (flag)
+				return true;		
+		}
+	}
+
+	return false;
 }
 
 
-
-
-
-
-
-int main(){
-	
+int main() {
 	vector<bool> nullablesVec;
-	
-	for(int i = 0; i < NONTERMINAL_ENUM_SIZE; ++i) {
-		
-		nullablesVec.push_back(nullablesVec[grammar[i]] || checkIfNullable(grammar[i]));
+
+	for (int i = 0; i < NONTERMINAL_ENUM_SIZE; ++i) {
+		nullablesVec.push_back(checkIfNullable((nonterminal)i));
 	}
-	
+
 	print_nullable(nullablesVec);
-	
-	
-	
+
 	return 0;
 }
